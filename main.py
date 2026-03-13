5d63bae2299c219c3cc04bd1a35bec5cb6cefed0
Hecker625 <kyler.shah@gmail.com>
2026-03-13 15:57:23 -0700

    Implement location confirmation and coordinate input
    Added user input handling for location confirmation and coordinate entry.

--- main.py
+++ main.py
@@ -37,3 +37,48 @@ periods = forecast_data["properties"]["periods"]
 for period in periods:
     print(period["name"])
     print(period["detailedForecast"])
+    print("\n")
+
+while True:
+    correct = input("\nIs this the correct location? ").lower()
+    if correct.startswith("y"):
+        print("Thank you for using the weather app.")
+        break
+
+    elif correct.startswith("n"):
+        coord = input("Enter the coordinates for your location (If you don't know your coordinites, go to https://latlong.net. Format is eg. 47.620422,-122.349358 ")
+        lat,lon = coord.split(",")
+        while True:
+            try:
+                print (f"The weather for the coordinates {lat}, {lon} is:")
+
+                # Step 2: ask NOAA what forecast grid this location belongs to
+                url = f"https://api.weather.gov/points/{lat},{lon}"
+
+                response = requests.get(url)
+
+                data = response.json()
+
+                # Step 3: get the forecast URL from the response
+                forecast_url = data["properties"]["forecast"]
+
+                # Step 4: request the forecast
+                forecast_response = requests.get(forecast_url)
+
+                forecast_data = forecast_response.json()
+
+                # Step 5: print the forecast periods
+                periods = forecast_data["properties"]["periods"]
+
+                for period in periods:
+                    print(period["name"])
+                    print(period["detailedForecast"])
+                    print("\n")
+                
+                break
+
+            except:
+                print("Coordinites not recognized. Please enter the coordinates in this format: latitude,longitude")
+
+    else:
+        print("Invalid input. Please try again.")
